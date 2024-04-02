f2bade09ff56da34de51f554d7e38e9d0a2622f5
Jonas-Hoang <66858257+Jonas-Hoang@users.noreply.github.com>
2024-04-02 08:57:19 +0700

    --- update main

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@
 </template>
 
 <script>
-import Header from "./components/Header/index";
-import BannerVid from "./components/Banner/index";
-import EventE from "./components/Event/index";
-import PurposeJ from "./components/Purpose/index";
+import Header from "./components/HeaderBar/index.vue";
+import BannerVid from "./components/BannerSection/index.vue";
+import AdterviseSection from "./components/AdvertiseSection/index.vue";
+import PurposeJ from "./components/PurposeJoin/index.vue";
 import SignUp from "./components/SignUp/index.vue";
 import EventMap from "./components/EventMap/index.vue";
 import TermSection from "./components/TermSection/index.vue";
