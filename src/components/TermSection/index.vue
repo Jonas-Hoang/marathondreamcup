d298b0d96c5a7ff54c13e7cb949a4de3573d1fe9
Jonas-Hoang <66858257+Jonas-Hoang@users.noreply.github.com>
2024-04-15 19:24:40 +0700

    feat: update image

--- src/components/TermSection/index.vue
+++ src/components/TermSection/index.vue
@@ -10,16 +10,16 @@
             <div class="col medium-5 small-12 large-7" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500" data-aos-offset="200">
               <h2 v-if="!$isMobile()" class="font-bold uppercase size italic text-4xl text-start mb-4">ĐIỀU KHOẢN & ĐIỀU KIỆN</h2>
               <h2 v-if="$isMobile()" class="font-bold uppercase size italic text-4xl text-center mb-4">ĐIỀU KHOẢN & ĐIỀU KIỆN</h2>
-              <p class="text-[19px] font-[300]">Running Diamond Cup 2024</p>
+              <p class="text-[19px] font-[300] !text-2xl">Running Diamond Cup 2024</p>
               <a class="button bg-[#ddc180] my-[20px] text-black px-[1.2em] max-w-[100%] text-[1.12em]" rel="noopener noreferrer" href="https://drive.google.com/file/d/1SVUsMCFWrEGW6PjaXa3WlL-VS21sKH8S/view" target="_blank" style="border-radius: 99px"><span>Đọc kỹ điều khoản &amp; điều kiện</span><i class="icon-angle-right" aria-hidden="true"></i></a>
               <div class="ux-timer">
                 <span>{{totalHours}}<br /><strong>giờ</strong></span><span>{{minutes}}<br /><strong>phút</strong></span><span>{{seconds}}<br /><strong>giây</strong></span>
               </div>
-              <span class="text-[400] mt-[15px] text-[22px] mb-[15px]">Đếm ngược tới ngày sự kiện diễn ra với</span>
-              <div class="flex items-center justify-center lg:w-[80%] md:w-[90%] sm:[100%] px-[30px] mt-[10px]">
+              <span class="text-[400] mt-[15px] text-[22px] mb-[10px]">Đếm ngược tới ngày sự kiện diễn ra với</span>
+              <div class="flex items-center justify-center lg:w-[80%] md:w-[90%] sm:[100%] px-[30px] mt-[5px]">
                 <span class="text-[60px] font-bold">5.000+</span>
               </div>
-              <span class="mt-[10px] text-[400] text-[22px]">vận động viên (dự kiến)</span>
+              <span class="mt-[10px] text-[400] text-[22px] pb-[10px]">vận động viên (dự kiến)</span>
             </div>
             <div class="col medium-5 small-12 large-5 " style="paddingp-bottom: 0 !important;" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500">
               <div class="flex flex-col justify-center">
