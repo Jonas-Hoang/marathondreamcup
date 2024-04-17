ddcaed378c7f3bf321f5d14ef37a6e3aa142a17b
NenPhan <thanhnen1360@gmail.com>
2024-04-17 13:22:54 +0700

    [UI] Event map - update poster

--- src/components/EventMap/index.vue
+++ src/components/EventMap/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="bg-gray-800 text-gray-100 relative">
-    <div class="bg section-bg fill ">
+    <div class="bg section-bg fill">
       <div class="section-bg-overlay absolute fill">
         <div class="content">
           <div class="row">
             <Carousel :v-bind="settings" :breakpoints="breakpoints">
               <Slide v-for="slide in slides" :key="slide.id">
-                <div class="col" data-aos="fade-right" data-aos-duration="1000" data-aos-offset="200">
+                <div
+                  class="col"
+                  data-aos="fade-right"
+                  data-aos-duration="1000"
+                  data-aos-offset="200"
+                >
                   <h2>{{ slide.title }}</h2>
                   <img :src="slide.url" alt="so do duong chay" />
                 </div>
@@ -25,8 +30,8 @@
 
 <script setup>
 import { ref } from "vue";
-import map5 from "../../../assets/images/map5km.jpeg";
-import map10 from "../../../assets/images/map10km.jpeg";
+import map5 from "../../../assets/images/map5km.jpg";
+import map10 from "../../../assets/images/map10km.jpg";
 import schedule from "../../../assets/images/schedule.jpeg";
 
 const settings = ref({
