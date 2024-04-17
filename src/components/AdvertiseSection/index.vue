a1307b27b7d01a0f333a32080328528d0b75a619
Jonas-Hoang <66858257+Jonas-Hoang@users.noreply.github.com>
2024-04-17 09:24:39 +0700

    --- css

--- src/components/AdvertiseSection/index.vue
+++ src/components/AdvertiseSection/index.vue
@@ -15,7 +15,7 @@
                 <h2 class="font-bold uppercase size italic text-4xl text-start mb-4 !text-[#fdb912]">
                   Sải bước chiến thắng cùng
                 </h2>
-                <div class="flex items-center justify-center lg:w-[80%] md:w-[90%] sm:[100%] mb-6 px-[30px]">
+                <div class="flex items-center justify-center mb-6 px-[30px]">
                   <span class="text-[#214184] my-[15px] text-3xl font-bold">Running Diamond Cup 2024</span>
                 </div>
                 <span class="text-[#214184] font-[400] text-lg mb-6" style="font-size: 110%">
@@ -37,13 +37,13 @@
             </div>
             <div v-if="$isMobile()" class="col medium-5 small-12 large-7" data-aos="fade-up" data-aos-duration="1000">
               <div class="flex flex-col justify-center">
-                <h2 class="font-bold uppercase size italic text-4xl mb-4 text-[#fdb912] text-center">
+                <h2 class="font-semibold uppercase size italic text-4xl mb-4 text-[#fdb912] text-center">
                   Sải bước chiến thắng cùng
                 </h2>
-                <div class="flex items-center justify-center lg:w-[80%] md:w-[90%] sm:[100%] mb-6 px-[30px]">
-                  <span class="text-center text-[#214184] my-[15px] text-3xl font-bold">Running Diamond Cup 2024</span>
+                <div class="flex items-center justify-center mb-6 px-[30px]">
+                  <span class="text-center text-[#214184] my-[15px] text-3xl font-semibold">Running Diamond Cup 2024</span>
                 </div>
-                <span class="text-[#214184] font-[400] text-lg mb-6" style="font-size: 110%">
+                <span class="text-[#214184] font-[300] text-lg mb-6" style="font-size: 110%">
                   Running Diamond Cup 2024 sẽ có 02 cự ly, gồm 5km và 10km dành
                   cho các vận động viên phong trào nam và nữ. Cung đường chạy
                   của giải đấu với những con đường mang vẻ đẹp nên thơ, vừa chất
@@ -51,7 +51,7 @@
                   chắn sẽ gợi nên những rung cảm và tạo được những dấu ấn khó
                   quên cho Runners.
                 </span>
-                <span class="text-[#214184] font-[400] text-lg" style="font-size: 110%">
+                <span class="text-[#214184] font-[300] text-lg" style="font-size: 110%">
                   Khi đăng ký Running Diamond Cup 2024 là bạn đang góp thêm hạnh
                   phúc đến mọi người, vì với lệ phí từ mỗi một thành viên tham
                   gia, Diamond Network sẽ trao đến Hội Liên hiệp Thanh niên Việt
