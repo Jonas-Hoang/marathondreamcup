afaef47b39205510b20c9accb3e287b8736295c2
NenPhan <thanhnen1360@gmail.com>
2024-04-24 22:36:14 +0700

    fix event map img height

--- src/components/EventMap/index.vue
+++ src/components/EventMap/index.vue
@@ -2,26 +2,24 @@
   <div class="bg-gray-800 text-gray-100 relative">
     <div class="bg section-bg fill">
       <div class="section-bg-overlay absolute fill">
-        <div class="content">
-          <div class="row">
-            <Carousel :v-bind="settings" :breakpoints="breakpoints">
-              <Slide v-for="slide in slides" :key="slide.id">
-                <div
-                  class="col"
-                  data-aos="fade-right"
-                  data-aos-duration="1000"
-                  data-aos-offset="200"
-                >
-                  <h2>{{ slide.title }}</h2>
-                  <img :src="slide.url" alt="so do duong chay" />
-                </div>
-              </Slide>
-              <template #addons>
-                <!-- <Navigation /> -->
-                <!-- <Pagination /> -->
-              </template>
-            </Carousel>
-          </div>
+        <div class="row">
+          <Carousel :v-bind="settings" :breakpoints="breakpoints">
+            <Slide v-for="slide in slides" :key="slide.id">
+              <div
+                class="col"
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                data-aos-offset="200"
+              >
+                <h2>{{ slide.title }}</h2>
+                <img :src="slide.url" alt="so do duong chay" />
+              </div>
+            </Slide>
+            <template #addons>
+              <!-- <Navigation /> -->
+              <!-- <Pagination /> -->
+            </template>
+          </Carousel>
         </div>
       </div>
     </div>
