0ec7da3067f83fa1bc2ae0d3c315bdb0f9096995
NenPhan <thanhnen1360@gmail.com>
2024-07-06 20:18:33 +0700

    [fix] find picture: change embed link to 2024

--- src/views/find-picture.vue
+++ src/views/find-picture.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <iframe src="https://embed.enjoysport.vn/running-diamon-cup-2024" name="iframe_a" title="Running Diamond Cup 2024" width="100%" height="1500 scrolling="></iframe>
+    <iframe src="https://enjoysport.vn/embed/running-diamon-cup-2024" name="iframe_a" title="Running Diamond Cup 2024" width="100%" height="1500 scrolling="></iframe>
   </div>
 </template>
 
